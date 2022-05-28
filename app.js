cb93ccde8571b7581840ac2c07d732919cf58954
Marcin <marcin.straniec@gmail.com>
2022-05-28 15:43:10 +0200

    summing up products added

---
+++ app.js
@@ -0,0 +1,74 @@
+const list = document.querySelector('ul');
+const addBtn = document.getElementById("addBtn");
+const allItems = document.getElementById("allItems");
+
+const inputElement = document.getElementById("textField");
+
+
+class Item {
+  constructor(itemName, count) {
+    this.itemName = itemName;
+    this.count = count;
+  }
+}
+
+isProduct = Boolean;
+
+let groceries = [];
+
+function addItem() {
+  const input = inputElement.value;
+  isProduct = false;
+
+  if(input === ""){
+    alert("enter some value");
+  }
+  else{
+    groceries.forEach(item => {
+      if(item.itemName === input){
+        item.count += 1;
+        isProduct = true;
+      }
+    })
+  
+    if(isProduct === false){
+      let newItem = new Item(itemName=input, count=1);
+      groceries.push(newItem);
+    }
+     reloadDisplay();
+  }
+
+}
+
+function reloadDisplay(){
+  let delBtn = document.createElement('button');
+  let editBtn = document.createElement('button');
+  let checkBtn = document.createElement('input');
+  delBtn.innerHTML = "-";
+  editBtn.innerHTML = "edit";
+  checkBtn.type = "checkbox";
+
+  list.innerText = ''
+  groceries.forEach(item => {
+    const newLi = document.createElement("li");
+    newLi.innerHTML += item.itemName + " x" + item.count;
+    list.insertAdjacentElement("beforeend", newLi);
+    newLi.appendChild(delBtn);
+    newLi.appendChild(editBtn);
+    newLi.appendChild(checkBtn);
+  })
+  inputElement.value = "";
+}
+
+addBtn.addEventListener('click', addItem);
+
+
+
+
+inputElement.addEventListener("keydown", function(event){
+  if(event.key === "Enter"){
+    addItem();
+    event.preventDefault(); 
+    return false;
+  }
+})
